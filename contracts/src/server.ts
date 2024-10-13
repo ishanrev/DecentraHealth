7193b3154fe4cb9be92ddd7fde13a9ec94d7ad5d
Ishan <ishanrev123@gmail.com>
2024-10-13 02:23:58 -0400

    basic functionalities done

--- contracts/src/server.ts
+++ contracts/src/server.ts
@@ -2,10 +2,11 @@ import express from 'express';
 import { proofManagement } from "./index";
 const app = express();
 const port = 3000; // You can change the port number if needed
-
+const cors = require('cors');
+app.use(cors())
 app.get('/trigger', async (req, res) => {
-  await proofManagement(); // Call the function from the other file
-  res.send('Proof has been sent for verification');
+  // await proofManagement(); // Call the function from the other file
+  res.send({data: 'Proof has been sent for verification'});
 });
 
 app.listen(port, () => {
